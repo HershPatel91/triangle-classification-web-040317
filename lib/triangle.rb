ecf715c4ce23fea5378814b70a6ea68ceca2d226
Hersh Patel <hershe.91@gmail.com>
2017-04-10 14:08:21 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
   # write code here
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+
+def kind
+  if @one + @two <= @three || @one + @three <= @two || @two + @three <= @one || @one <= 0 || @two <= 0 || @three <= 0
+        raise TriangleError
+    elsif @one == @two && @two == @three
+      :equilateral
+    elsif @one == @two || @two == @three || @three == @one
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
+
+class TriangleError < StandardError
+    def message 
+      "You must input a valid triangle!"
+    end
+  end  
+      
+      
